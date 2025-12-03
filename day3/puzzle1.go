dab4dbf5f692cf8e10fa9c3ff9be4623f532a785
Steve Calder <steve.r.calder@gmail.com>
2025-12-03 07:08:13 -0500

    day 3 completed!!

--- day3/puzzle1.go
+++ day3/puzzle1.go
@@ -1,6 +1,60 @@
 package day3
 
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/eaglesfrogs/adventofcode2025/util"
+)
+
 func (p *Puzzle) Puzzle1() error {
-	// Implementation for Puzzle 1
+	lines, err := util.ReadFileLines(p.InputFile)
+	if err != nil {
+		return err
+	}
+
+	totalJoltage := 0
+
+	for _, line := range lines {
+		firstHighest := '0'
+		firstHighestIdx := 0
+
+		for i, char := range line[0 : len(line)-1] {
+			if char == '9' {
+				firstHighest = '9'
+				firstHighestIdx = i
+				break
+			}
+
+			if char > firstHighest {
+				firstHighest = char
+				firstHighestIdx = i
+			}
+		}
+
+		secondHighest := '0'
+		for _, char := range line[firstHighestIdx+1 : len(line)] {
+			if char == '9' {
+				secondHighest = '9'
+				break
+			}
+
+			if char > secondHighest {
+				secondHighest = char
+			}
+		}
+
+		joltage, err := strconv.Atoi(fmt.Sprintf("%c%c", firstHighest, secondHighest))
+		if err != nil {
+			return err
+		}
+		log.Printf("Highest joltage is %d\n", joltage)
+
+		totalJoltage += joltage
+	}
+
+	log.Printf("Total joltage for puzzle 1 is %d", totalJoltage)
+
 	return nil
 }
--- day3/puzzle2.go
+++ day3/puzzle2.go
@@ -1,6 +1,52 @@
 package day3
 
+import (
+	"log"
+
+	"github.com/eaglesfrogs/adventofcode2025/util"
+)
+
 func (p *Puzzle) Puzzle2() error {
-	// Implementation for Puzzle 1
+	lines, err := util.ReadFileLines(p.InputFile)
+	if err != nil {
+		return err
+	}
+
+	var totalJoltage int64 = 0
+
+	for _, line := range lines {
+		var joltage int64 = 0
+
+		nextStartingIdx := 0
+		for i := 0; i < 12; i++ {
+			highest := '0'
+
+			idx := nextStartingIdx
+
+			for idx < len(line)-11+i {
+				if line[idx] == '9' {
+					highest = '9'
+					nextStartingIdx = idx + 1
+					break
+				}
+
+				if rune(line[idx]) > highest {
+					highest = rune(line[idx])
+					nextStartingIdx = idx + 1
+				}
+
+				idx++
+			}
+
+			joltage = joltage*10 + int64(highest-'0')
+		}
+
+		log.Printf("Highest voltage is %d\n", joltage)
+
+		totalJoltage += joltage
+	}
+
+	log.Printf("Total joltage for puzzle 2 is %d", totalJoltage)
+
 	return nil
 }

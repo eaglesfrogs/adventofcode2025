55ed26aee2db3d79c76e6788d0f851000980badb
Steve Calder <steve.r.calder@gmail.com>
2025-12-06 10:46:58 -0500

    day 6 done!

--- day6/puzzle1.go
+++ day6/puzzle1.go
@@ -1,6 +1,72 @@
 package day6
 
+import (
+	"log"
+	"regexp"
+	"strconv"
+
+	"github.com/eaglesfrogs/adventofcode2025/util"
+)
+
 func (p *Puzzle) Puzzle1() error {
-	// Implementation for Puzzle 1
+	lines, err := util.ReadFileLines(p.InputFile)
+	if err != nil {
+		return err
+	}
+
+	num0, err := extractNumbers(lines[0])
+	if err != nil {
+		return err
+	}
+	num1, err := extractNumbers(lines[1])
+	if err != nil {
+		return err
+	}
+	num2, err := extractNumbers(lines[2])
+	if err != nil {
+		return err
+	}
+	num3, err := extractNumbers(lines[3])
+	if err != nil {
+		return err
+	}
+	ops := extractOperators(lines[4])
+
+	total := int64(0)
+
+	for i := 0; i < len(num0); i++ {
+		if ops[i] == "+" {
+			total += num0[i] + num1[i] + num2[i] + num3[i]
+		} else {
+			total += num0[i] * num1[i] * num2[i] * num3[i]
+		}
+	}
+
+	log.Printf("Answer for puzzle 1 is %d", total)
+
 	return nil
+}
+
+func extractNumbers(line string) ([]int64, error) {
+	pattern := regexp.MustCompile(`\d+`)
+	matches := pattern.FindAllString(line, -1)
+
+	nums := make([]int64, len(matches))
+
+	var err error
+	for i := 0; i < len(matches); i++ {
+		nums[i], err = strconv.ParseInt(matches[i], 10, 64)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return nums, nil
+}
+
+func extractOperators(line string) []string {
+	pattern := regexp.MustCompile(`[+|*]`)
+	matches := pattern.FindAllString(line, -1)
+
+	return matches
 }
--- day6/puzzle2.go
+++ day6/puzzle2.go
@@ -1,6 +1,69 @@
 package day6
 
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+
+	"github.com/eaglesfrogs/adventofcode2025/util"
+)
+
 func (p *Puzzle) Puzzle2() error {
-	// Implementation for Puzzle 1
+	lines, err := util.ReadFileLines(p.InputFile)
+	if err != nil {
+		return err
+	}
+
+	ops := extractOperators(lines[4])
+	numbers := make([]int64, 0)
+	total := int64(0)
+
+	for i := 0; i < len(lines[0]); i++ {
+		if lines[0][i] == ' ' && lines[1][i] == ' ' && lines[2][i] == ' ' && lines[3][i] == ' ' {
+			ans := int64(0)
+			if ops[0] == "*" {
+				ans = int64(1)
+			}
+
+			for _, n := range numbers {
+				if ops[0] == "+" {
+					ans += n
+				} else {
+					ans *= n
+				}
+			}
+
+			ops = ops[1:]
+			total += ans
+			numbers = make([]int64, 0)
+
+			continue
+		}
+
+		numStr := strings.TrimSpace(fmt.Sprintf("%c%c%c%c", lines[0][i], lines[1][i], lines[2][i], lines[3][i]))
+		num, err := strconv.ParseInt(numStr, 10, 64)
+		if err != nil {
+			return err
+		}
+
+		numbers = append(numbers, num)
+	}
+
+	ans := int64(0)
+	if ops[0] == "*" {
+		ans = int64(1)
+	}
+	for _, n := range numbers {
+		if ops[0] == "+" {
+			ans += n
+		} else {
+			ans *= n
+		}
+	}
+	total += ans
+
+	log.Printf("Answer for puzzle 2 is %d", total)
+
 	return nil
 }

7d529c21cf66ff0664dc03c8bcc5b92c801be323
Steve Calder <steve.r.calder@gmail.com>
2025-12-08 08:32:28 -0500

    day 8 puzzle 1

--- day8/puzzle1.go
+++ day8/puzzle1.go
@@ -1,6 +1,108 @@
 package day8
 
+import (
+	"log"
+	"sort"
+	"strconv"
+	"strings"
+
+	"github.com/eaglesfrogs/adventofcode2025/util"
+)
+
 func (p *Puzzle) Puzzle1() error {
-	// Implementation for Puzzle 1
+	lines, err := util.ReadFileLines(p.InputFile)
+	if err != nil {
+		return err
+	}
+
+	points := make([]*Point, len(lines))
+
+	for i := 0; i < len(lines); i++ {
+		lineChunks := strings.Split(lines[i], ",")
+
+		x, err := strconv.ParseFloat(lineChunks[0], 64)
+		if err != nil {
+			return err
+		}
+		y, err := strconv.ParseFloat(lineChunks[1], 64)
+		if err != nil {
+			return err
+		}
+		z, err := strconv.ParseFloat(lineChunks[2], 64)
+		if err != nil {
+			return err
+		}
+
+		points[i] = &Point{x, y, z, make([]*Point, 0)}
+	}
+
+	pointDistances := make([]*PointDistance, 0)
+	for i := 0; i < len(points); i++ {
+		for j := i + 1; j < len(points); j++ {
+			p1 := points[i]
+			p2 := points[j]
+			dist := p1.calcDistance(p2)
+
+			pointDistances = append(pointDistances, &PointDistance{p1, p2, dist})
+		}
+	}
+
+	sort.Slice(pointDistances, func(i, j int) bool {
+		return pointDistances[i].distance < pointDistances[j].distance
+	})
+
+	pointDistances = pointDistances[0:1000]
+	for _, pointDistance := range pointDistances {
+		p1 := pointDistance.p1
+		p2 := pointDistance.p2
+
+		p1.connections = append(p1.connections, p2)
+		p2.connections = append(p2.connections, p1)
+	}
+
+	visited := make(map[*Point]bool)
+	graphLengths := make([]int, 0)
+
+	for _, point := range points {
+		if _, v := visited[point]; v {
+			continue
+		}
+
+		if len(point.connections) == 0 {
+			continue
+		}
+
+		count := 1
+		visited[point] = true
+		pointsToCheck := make([]*Point, 0)
+		pointsToCheck = append(pointsToCheck, point.connections...)
+
+		for len(pointsToCheck) > 0 {
+			nextPoint := pointsToCheck[0]
+			pointsToCheck = pointsToCheck[1:]
+
+			if _, v := visited[nextPoint]; v {
+				continue
+			}
+
+			count++
+			visited[nextPoint] = true
+
+			for _, c := range nextPoint.connections {
+				if _, v := visited[c]; !v {
+					pointsToCheck = append(pointsToCheck, c)
+				}
+			}
+		}
+
+		graphLengths = append(graphLengths, count)
+	}
+
+	sort.Slice(graphLengths, func(i, j int) bool {
+		return graphLengths[i] > graphLengths[j]
+	})
+
+	log.Printf("The answer to puzzle 1 is %d", graphLengths[0]*graphLengths[1]*graphLengths[2])
+
 	return nil
 }
--- day8/puzzleSetup.go
+++ day8/puzzleSetup.go
@@ -1,6 +1,10 @@
 package day8
 
-import "github.com/eaglesfrogs/adventofcode2025/util"
+import (
+	"math"
+
+	"github.com/eaglesfrogs/adventofcode2025/util"
+)
 
 type Puzzle struct {
 	InputFile string
@@ -8,4 +12,26 @@ type Puzzle struct {
 
 func NewPuzzle(inputFile string) util.DailyPuzzle {
 	return &Puzzle{InputFile: inputFile}
+}
+
+type Point struct {
+	x           float64
+	y           float64
+	z           float64
+	connections []*Point
+}
+
+func (p *Point) calcDistance(p2 *Point) float64 {
+	x := p2.x - p.x
+	y := p2.y - p.y
+	z := p2.z - p.z
+	sum := x*x + y*y + z*z
+
+	return math.Sqrt(sum)
+}
+
+type PointDistance struct {
+	p1       *Point
+	p2       *Point
+	distance float64
 }
